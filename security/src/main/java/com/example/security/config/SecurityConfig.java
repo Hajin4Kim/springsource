4db288e66667bb2bb7a2df869564a55230be0599
kate <khj020416@gmail.com>
2024-11-20 11:58:17 +0900

    Spring 19일차 : Security 활용한 기본 프로젝트 초반 설정

--- security/src/main/java/com/example/security/config/SecurityConfig.java
+++ security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity // TODO: 웹에 적용할 시큐리티 클래스 포함
 @Configuration // TODO: 환경설정 파일
@@ -23,10 +24,15 @@ public class SecurityConfig {
     http
         .authorizeHttpRequests(authorize -> authorize
             // TODO: "/" 경로에 대해 permission All 로 바꿈
-            .requestMatchers("/", "/sample/guest").permitAll()
+            .requestMatchers("/", "/sample/guest", "/auth").permitAll()
             .requestMatchers("/sample/member").hasRole("USER")
             .requestMatchers("/sample/admin").hasRole("ADMIN"))
-        .formLogin(Customizer.withDefaults());
+        // .formLogin(Customizer.withDefaults()); TODO: 시큐리티가 제공하는 로그인 페이지
+        // TODO: 내가 만든 로그인 페이지 띄우기
+        .formLogin(login -> login.loginPage("/member/login").permitAll())
+        .logout(logout -> logout
+            .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+            .logoutSuccessUrl("/"));
 
     return http.build();
   }
@@ -43,6 +49,12 @@ public class SecurityConfig {
         .password("{bcrypt}$2a$10$t8eaMZpHy/1KwzyQ3TjGeedRhFkjrws5Cu1/Nl4mlQMaSPLr2tSB.")
         .roles("USER")
         .build();
-    return new InMemoryUserDetailsManager(user);
+
+    UserDetails admin = User.builder()
+        .username("admin")
+        .password("{bcrypt}$2a$10$t8eaMZpHy/1KwzyQ3TjGeedRhFkjrws5Cu1/Nl4mlQMaSPLr2tSB.")
+        .roles("ADMIN", "USER")
+        .build();
+    return new InMemoryUserDetailsManager(user, admin);
   }
 }
--- security/src/main/java/com/example/security/controller/HomeController.java
+++ security/src/main/java/com/example/security/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,9 +11,19 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Controller
 public class HomeController {
+
   @GetMapping("/")
   public String home() {
     log.info("home 요청");
     return "home";
+  }
+
+  @GetMapping("/auth")
+  public Authentication getAuthentication() { // TODO: springframework.security.core
+    SecurityContext context = SecurityContextHolder.getContext();
+    Authentication authentication = context.getAuthentication();
+
+    log.info("getAuthentication 요청");
+    return authentication;
   }
 }
