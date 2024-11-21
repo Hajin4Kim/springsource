780ba7da7d62bd04f277eeded7bf0e003e8e6842
kate <khj020416@gmail.com>
2024-11-21 18:39:23 +0900

    Spring 20일차 : Social 로그인 작업

--- club/src/main/java/com/example/club/controller/HomeController.java
+++ club/src/main/java/com/example/club/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.club.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,6 +13,7 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 public class HomeController {
 
+    @PreAuthorize("permitAll()") // TODO: 접근제한 설정
     @GetMapping("/")
     public String getHome() {
         return "home";
